ae70a16ab7a7be45a6e8b3745c81cdbbb2a28f39
C456 <C456@C456-PC>
2018-11-30 17:51:15 +0900

    opera driver

--- FirstPj/src/com/ygc/PxyWebDriver.java
+++ FirstPj/src/com/ygc/PxyWebDriver.java
@@ -2,49 +2,52 @@ package com.ygc;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PxyWebDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		PxyWebDriver pwd = new PxyWebDriver();
-		pwd.test();
+		pwd.test2();
 	}
 	
 	@SuppressWarnings("deprecation")
 	public void test() {
+		String exePath = "C:\\workspace\\selenium\\FirstPj\\exe\\chromedriver_win32\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		
 		WebDriver driver = null;
 		try {
-			String exePath = "C:\\workspace\\selenium\\FirstPj\\exe\\chromedriver_win32\\chromedriver.exe";
-			System.setProperty("webdriver.chrome.driver", exePath);
 			
 //			ChromeOptions chromeOptions = new ChromeOptions();
 ////		    chromeOptions.setBinary("/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary");
 //		    chromeOptions.addArguments("headless");
 //		    driver = new ChromeDriver(chromeOptions);
-		      
+		    
+			String host = "218.239.244.15";
+			String port = "80";
 
-			String PROXY = "localhost:8080";
-			PROXY = "88.148.92.37:8080";
-
-			org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
-			proxy.setHttpProxy(PROXY)
-			     .setFtpProxy(PROXY)
-			     .setSslProxy(PROXY);
-			DesiredCapabilities cap = new DesiredCapabilities();
-			cap.setCapability(CapabilityType.PROXY, proxy);
-
-			driver = new ChromeDriver(cap);
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--proxy-server=\"socks5://" + host + ":" + port+"\"");
+			options.addArguments("--host-resolver-rules=\"MAP * ~NOTFOUND , EXCLUDE " + host +"\"");
+			driver = new ChromeDriver(options);
 			
 	        //Launch the Online Store Website
-			driver.get("https://whatismyipaddress.com/ip-lookup");
+			driver.get("https://www.whatismyip.com/my-ip-information/");
+//			driver.get("https://www.whatismyip.com/my-ip-information/");
 
 	        
 		} catch (Exception e) {
@@ -57,4 +60,35 @@ public class PxyWebDriver {
 		}
     }		
 
+	
+	public void test2() {
+		String exePath = "C:\\workspace\\selenium\\FirstPj\\exe\\geckodriver-v0.23.0-win64\\geckodriver.exe";
+		System.setProperty("webdriver.gecko.driver", exePath);
+				
+		
+		String host = "218.239.244.15";
+		String port = "80";
+		
+//		host = "166.88.124.111";
+//		port = "3111";
+		String proxy = host;
+		
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		capability.setJavascriptEnabled(true);
+		FirefoxProfile profile = new FirefoxProfile();
+
+		profile.setPreference("network.proxy.type", 1);
+		profile.setPreference("network.proxy.http", proxy);
+		profile.setPreference("network.proxy.http_port", port);
+		profile.setPreference("network.proxy.ssl", proxy);
+		profile.setPreference("network.proxy.ssl_port", port);
+		profile.setPreference("dom.ipc.plugins.enabled.libflashplayer.so", false);
+
+		capability.setCapability("firefox_profile", profile);
+		WebDriver driver = new FirefoxDriver(capability);		
+
+		driver.get("https://www.iplocation.net/find-ip-address");
+		
+		
+	}
 }
